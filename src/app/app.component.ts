fbcc0998f1fa2ac516daa6b512b1a59cf61efda9
GomaGoma676 <pga.adam.scott@gmail.com>
2020-03-30 11:11:11 +0900

    newly created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faHorse } from '@fortawesome/free-solid-svg-icons';
+import { faRecycle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ScrumTaskBoard';
+  faHorse = faHorse
+  faRecycle = faRecycle
 }
